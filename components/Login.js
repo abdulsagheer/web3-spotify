e154cbbdbcda1463270fbdbd38b498f026f3bf11
abdulsagheer <abdulsagheeras29@gmail.com>
2022-12-07 00:52:07 +0530

    init

--- components/Login.js
+++ components/Login.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import { useWallet } from "@solana/wallet-adapter-react";
+import Payment from "./Payment";
 
 const styles = {
   loginPage: `w-screen h-screen bg-white flex justify-center flex-col items-center`,
@@ -7,6 +9,10 @@ const styles = {
 };
 
 const Login = () => {
+  const wallet = useWallet();
+
+  if (wallet.connected) return <Payment />;
+
   return (
     <div className={styles.loginPage}>
       <p className={styles.text}>Login to access this app</p>
